97285d3bb594b469e35866decd882e8037e79cfd
Bastien <bastienrbt@protonmail.com>
2018-01-07 18:11:43 +0100

    Set the good date for the countdown

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,8 +1,8 @@
 <template>
   <section>
-     <img src="../assets/logo.svg" alt="Logo Capsule Bordeaux - E-shop éphémère des créateurs Bordelais" id="logo">
-     <h2 class="comingSoon">Capsule arrive dans</h2>
-    <Countdown date="December 30, 2018"></Countdown>
+    <img src="../assets/logo.svg" alt="Logo Capsule Bordeaux - E-shop éphémère des créateurs Bordelais" id="logo">
+    <h2 class="comingSoon">Capsule arrive dans</h2>
+    <Countdown date="January 28, 2018"></Countdown>
 
     <div class="newsletter">
       <h3>Inscrivez-vous à notre newsletter pour être notifiez lors de sa sortie !</h3>
